bbffb6dc5d84aca188cc2c295bbca2b29418fabd
MagistrAVSH <art046@hotmail.com>
2019-12-27 07:50:33 +0300

    update

--- Map/Things/IThing.cs
+++ Map/Things/IThing.cs
@@ -25,8 +25,8 @@ namespace Magistr.Things
     public enum MoveDirection
     {
         None = 0,
-        Forward = 1 << 0,
-        Backward = 1 << 1,
+        RollLeft = 1,
+        RollRight = 1 << 1,
         Left = 1 << 2,
         Right = 1 << 3,
         Up = 1 << 4,
